39cc51abf005e8dfd2b5ca1182b768020e91824a
carriera <tomaszlasota@gmail.com>
2016-05-04 15:17:19 +0200

    Update BehaviorFilter.php

--- BehaviorFilter.php
+++ BehaviorFilter.php
@@ -28,7 +28,7 @@ class BehaviorFilter extends \yii\base\Behavior {
 	 */
 	public function events()
 	{
-		return [Controller::EVENT_BEFORE_ACTION => 'beforeAction'];
+		return 'beforeAction';
 	}
 
 	/**
